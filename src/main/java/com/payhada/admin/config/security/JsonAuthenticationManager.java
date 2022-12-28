be6e797da121a93139ded82c81c9019672452980
sw.jeon <sw.jeon@payhada.com>
2022-12-28 17:53:24 +0900

    로그인 서비스 구현

--- src/main/java/com/payhada/admin/config/security/JsonAuthenticationManager.java
+++ src/main/java/com/payhada/admin/config/security/JsonAuthenticationManager.java
@@ -1,48 +1,61 @@
 package com.payhada.admin.config.security;
 
+import com.payhada.admin.common.util.SHAEncryption;
 import com.payhada.admin.model.LoginDTO;
 import com.payhada.admin.service.user.LoginService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONObject;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 //@Component
-@RequiredArgsConstructor
 @Slf4j
 @Component
 public class JsonAuthenticationManager implements AuthenticationManager {
+
     private final LoginService loginService;
+
+    public JsonAuthenticationManager(LoginService loginService) {
+        this.loginService = loginService;
+    }
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        Authentication auth = null;
         log.debug("JsonAuthenticationManager is running...");
-        String username, password = null;
+
+        Authentication auth = null;
+
         LoginDTO loginParam = new LoginDTO();
-        LoginDTO loginResult = null;
+        LoginDTO loginResult;
         ArrayList<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
         try {
-            username = authentication.getPrincipal().toString();
-            password = authentication.getCredentials().toString();
+            String username = authentication.getPrincipal().toString();
+            String password = authentication.getCredentials().toString();
             loginParam.setId(username);
-            loginParam.setPwd(password);
+//            loginParam.setPwd(password);
             loginResult = loginService.loginWithLoginIdAndPwd(loginParam);
-            if(loginResult != null) {
-                authorities.add(new SimpleGrantedAuthority("TMP")); //TODO add user role
-                auth = new UsernamePasswordAuthenticationToken(loginResult, null, authorities);
-            }
+
+            if (loginResult == null) throw new UsernameNotFoundException("아이디를 찾을 수 없습니다.");
+
+            if (!SHAEncryption.encrypt512(password).equals(loginResult.getPwd())) throw new BadCredentialsException("패스워드가 일치하지 않습니다.");
+
+            List<SimpleGrantedAuthority> employeeAuthorities = loginService.getEmployeeAuthorities(loginResult);
+            authorities.addAll(employeeAuthorities);
+
+            auth = new UsernamePasswordAuthenticationToken(loginResult.getUserNo(), loginResult.getPwd(), authorities);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return auth;
     }
 
