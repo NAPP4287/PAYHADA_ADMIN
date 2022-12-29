92eef01008fbbcec6695d7fe63e0492fb39312a7
이나은 <lee-naeun@ina-eun-ui-MacBookPro-2136.local>
2022-12-29 16:46:26 +0900

    typescript로 초기화 및 셋팅

---
+++ frontend/admin/src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import Footer from "components/Footer";
+import { Button } from "reactstrap";
+
+function Login() {
+  return (
+    <div>
+      <h1>this is login page</h1>
+      <Footer />
+      <Button color="danger">버튼입니당!</Button>
+    </div>
+  );
+}
+
+export default Login;
