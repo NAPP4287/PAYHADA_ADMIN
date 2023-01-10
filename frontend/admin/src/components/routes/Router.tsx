999bc1008d992dfb3e633626ca8853a4e392283b
hyeonji.tak <hyeonji.tak@payhada.com>
2023-01-10 15:13:21 +0900

    [FE] fix/spring+react build setting 오류 해결
    - build.gradle > npm install 후 build 되게 단계 추가
    - Router 대문자 P 오류 수정
    - package.json 프로젝트명 수정

--- frontend/admin/src/components/routes/Router.tsx
+++ frontend/admin/src/components/routes/Router.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "pages/Login";
 import Main from "pages/Main";
-import PrivateRoute from "./PrivateRoute";
+import PrivateRoute from "./privateRoute";
 
 const Router = () => {
   return (
