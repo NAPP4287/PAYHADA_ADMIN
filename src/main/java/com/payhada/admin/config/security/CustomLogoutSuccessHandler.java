b9574cd51725802a943798f071d0b4fe34d60979
sw.jeon <sw.jeon@payhada.com>
2023-01-31 17:43:43 +0900

    Spring Security 로그아웃 구현, 인증/인가 예외 처리 추가

---
+++ src/main/java/com/payhada/admin/config/security/CustomLogoutSuccessHandler.java
@@ -0,0 +1,34 @@
+package com.payhada.admin.config.security;
+
+import com.payhada.admin.common.setting.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        response.setStatus(HttpStatus.OK.value());
+
+        Response responseDTO = Response.builder()
+                .resultCode(200)
+                .resultMsg("로그아웃 성공")
+                .build();
+
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        MediaType jsonMimeType = MediaType.APPLICATION_JSON;
+
+        if (jsonConverter.canWrite(responseDTO.getClass(), jsonMimeType)) {
+            jsonConverter.write(responseDTO, jsonMimeType, new ServletServerHttpResponse(response));
+        }
+    }
+}
